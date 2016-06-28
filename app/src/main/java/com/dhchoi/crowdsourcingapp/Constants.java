273f1a7b0c4efc1037c983f51c8cf8a0eb8558ef
TPeterW <peterwangtao0@hotmail.com>
2016-06-28 16:28:38 -0400

    Took Constants.java out of .gitignore

---
+++ app/src/main/java/com/dhchoi/crowdsourcingapp/Constants.java
@@ -0,0 +1,25 @@
+package com.dhchoi.crowdsourcingapp;
+
+public class Constants {
+
+    private Constants() {
+    }
+
+    public static final String TAG = "CrowdSourcingApp";
+    public static final String PACKAGE_NAME = "com.dhchoi.crowdsourcingapp";
+    public static final String DEFAULT_SHARED_PREF = PACKAGE_NAME + ".DEFAULT_SHARED_PREF";
+
+    public static final String APP_SERVER_BASE_URL = "http://ec2-54-221-193-1.compute-1.amazonaws.com:3000";
+//    public static final String APP_SERVER_BASE_URL = "http://10.0.2.2:3000";
+    public static final String APP_SERVER_TASK_CREATE_URL = APP_SERVER_BASE_URL + "/db/task-add";
+    public static final String APP_SERVER_TASK_COMPLETE_URL = APP_SERVER_BASE_URL + "/db/task-respond";
+    public static final String APP_SERVER_TASK_SYNC_URL = APP_SERVER_BASE_URL + "/db/task-sync";
+    public static final String APP_SERVER_TASK_FETCH_URL = APP_SERVER_BASE_URL + "/db/task-fetch";
+    public static final String APP_SERVER_TASK_DELETE_URL = APP_SERVER_BASE_URL + "/db/task-delete";
+    public static final String APP_SERVER_RESPONSE_FETCH_URL = APP_SERVER_BASE_URL + "/db/response-fetch";
+    public static final String APP_SERVER_USER_CREATE_URL = APP_SERVER_BASE_URL + "/db/user-create";
+    public static final String APP_SERVER_USER_FETCH_URL = APP_SERVER_BASE_URL + "/db/user-fetch";
+
+    private static final String GOOGLE_API_KEY = "AIzaSyCQxcGQCyk6XyUoJcuFNLmC5ddNR0xJAu8";
+
+}
