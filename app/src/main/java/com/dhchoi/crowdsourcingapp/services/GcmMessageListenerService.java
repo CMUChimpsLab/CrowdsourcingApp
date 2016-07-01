a2d6774e602f85d3319db299ea9408512a8dd089
TPeterW <peterwangtao0@hotmail.com>
2016-07-01 16:13:45 -0400

    Added GCM filtering

--- app/src/main/java/com/dhchoi/crowdsourcingapp/services/GcmMessageListenerService.java
+++ app/src/main/java/com/dhchoi/crowdsourcingapp/services/GcmMessageListenerService.java
@@ -5,6 +5,7 @@ import android.util.Log;
 
 import com.dhchoi.crowdsourcingapp.NotificationHelper;
 import com.dhchoi.crowdsourcingapp.activities.MainActivity;
+import com.dhchoi.crowdsourcingapp.user.UserManager;
 import com.google.android.gms.gcm.GcmListenerService;
 
 public class GcmMessageListenerService extends GcmListenerService {
@@ -18,39 +19,27 @@ public class GcmMessageListenerService extends GcmListenerService {
      * @param data Data bundle containing message data as key/value pairs.
      *             For Set of keys use data.keySet().
      */
-    // [START receive_message]
     @Override
     public void onMessageReceived(String from, Bundle data) {
         Log.d(TAG, "From: " + from);
         Log.d(TAG, "Bundle Data: " + data);
 
-        if (from.startsWith("/topics/")) {
-            // message received from some topic.
-        } else {
-            // normal downstream message.
+        // taskOwnerId, taskName, messageType
+        try {
+            String ownerId = data.getString("task");
+            String type = data.getString("messageType");
+
+            assert ownerId != null;
+            assert type != null;
+
+            if (type.equals("created") && !ownerId.equals(UserManager.getUserId(this))) {
+                NotificationHelper.createNotification("New Task Available", data.getString("taskName", "Touch to check new task."), this, MainActivity.class);
+            } else if (type.equals("updated") && ownerId.equals(UserManager.getUserId(this))) {
+                NotificationHelper.createNotification("Someone responded to your task", data.getString("taskName", "Touch to check response"), this, MainActivity.class);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
 
-        // [START_EXCLUDE]
-        /**
-         * Production applications would usually process the message here.
-         * Eg: - Syncing with server.
-         *     - Store message in local database.
-         *     - Update UI.
-         */
-//        simpleGeofenceManager.setGeofence(id,
-//                name,
-//                lat,
-//                lng,
-//                radius,
-//                GEOFENCE_EXPIRATION_TIME,
-//                Geofence.GEOFENCE_TRANSITION_ENTER | Geofence.GEOFENCE_TRANSITION_EXIT);
-
-        /**
-         * In some cases it may be useful to show a notification indicating to the user
-         * that a message was received.
-         */
-        NotificationHelper.createNotification("New task available", data.getString("name", "Touch to check new task."), this, MainActivity.class);
-        // [END_EXCLUDE]
     }
-    // [END receive_message]
 }
