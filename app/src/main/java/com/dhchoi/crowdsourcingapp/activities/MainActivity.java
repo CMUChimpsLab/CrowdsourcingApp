14787bc34319c362365524ed1bebe32e5a0bb30c
TPeterW <peterwangtao0@hotmail.com>
2016-07-28 15:35:55 -0400

    Removed unused import

--- app/src/main/java/com/dhchoi/crowdsourcingapp/activities/MainActivity.java
+++ app/src/main/java/com/dhchoi/crowdsourcingapp/activities/MainActivity.java
@@ -21,7 +21,6 @@ import android.support.v4.content.LocalBroadcastManager;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.Toolbar;
-import android.text.method.BaseKeyListener;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
